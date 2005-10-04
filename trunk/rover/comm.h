375b1391a56340dcb1c23007cb525f1c496dc348
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2005-10-04 16:05:48 +0000

    Added comments

--- trunk/rover/comm.h
+++ trunk/rover/comm.h
@@ -1,22 +1,27 @@
-/*comm.h - communication header module thingie
-*Author: Phil Hurwitz & Programming Team (eXtreme style)
-*Created: 29 Sep 2005
-*Modified: 29 Sep 2005
+/*
+*  comm.h - communication header module thingie
+*  Created: 29 Sep 2005
+*  $Id$
 */
 
 #ifndef COMM_H
 #define COMM_H
 
+// Flags in each communication packet
 #define DIRECTION	0x01
 
+/*
+*  Standard packet for transmitting data
+*  across a link (serial, RF, etc)
+*/ 
 struct comm_protocol
 {
-	char start_code;
-	unsigned char device;
-	unsigned char magnitude;
-	char flags;
-	char checksum; //7-bit checksum & 1-bit checksum parity bit
-	char end_code;
+	char start_code;		// Constant start byte
+	unsigned char device;		// Device type
+	unsigned char magnitude;	// Device-dependant (i.e. speed of a motor)
+	char flags;			// 
+	char checksum;			// 7-bit checksum & 1-bit checksum parity bit
+	char end_code;			// Constant end byte
 };
 
 #endif
