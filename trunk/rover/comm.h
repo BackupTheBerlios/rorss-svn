179ef3f3a55847efcc7f3b3c9535b112f9a95fd6
julians <julians@f5ad59e7-92ee-0310-b632-925698422abb>
2005-10-07 02:12:52 +0000

    Added IE masks

--- trunk/rover/comm.h
+++ trunk/rover/comm.h
@@ -7,6 +7,17 @@
 #ifndef COMM_H
 #define COMM_H
 
+//Ie (0xa8) interrupt Enable Register
+#define IE_GLOBAL_INTERRUPT	(0x80)
+#define IE_SERIAL_PORT_1	(0x40)
+#define IE_SERIAL_PORT_0	(0x10)
+#define IE_TIMER_1		(0x08)
+#define IE_EXTERNAL_1		(0x04)
+#define IE_TIMER_0		(0x02)
+#define IE_EXTERNAL_0		(0x01)
+
+
+
 // Flags in each communication packet
 #define DIRECTION	0x01
 
