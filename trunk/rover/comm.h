b47908e451311d7db5e6762fe3b00bc0fde921b4
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2005-10-07 03:16:05 +0000

    Added CRITICAL_SECTION macros

--- trunk/rover/comm.h
+++ trunk/rover/comm.h
@@ -7,7 +7,18 @@
 #ifndef COMM_H
 #define COMM_H
 
-//Ie (0xa8) interupt Enable Register
+#include "loc.h"
+
+// Macros
+// Disable all interrupts (except flash/debug)
+#define BEGIN_CRITICAL_SECTION() \
+	(IE &= 0x7F)
+
+// Enable all interrupts
+#define END_CRITICAL_SECTION() \
+	(IE |= 0x80)
+
+//IE (0xa8) interupt Enable Register
 //P 29 of  http://www.chipcon.com/files/CC1010_Data_Sheet_1_3.pdf
 #define IE_EA		(0x80)//GLOBAL_INTERRUPT
 #define IE_ES1		(0x40)//SERIAL_PORT_1
@@ -16,6 +27,7 @@
 #define IE_TX1		(0x04)//EXTERNAL_1
 #define IE_ET0		(0x02)//TIMER_0
 #define IE_TX0		(0x01)//EXTERNAL_0
+
 //EIE (0xE8) Extended Interrupt Enable Register
 #define EIE_RTCIE	(0x10)//REALTIME_CLOCK
 #define EIE_ET3		(0x08)//TIMER_3
@@ -29,7 +41,7 @@
 #define EXIF_RFIF	(0x10)//RF Transmit/receive interrupt flag
 
 // Flags in each communication packet
-#define DIRECTION	0x01
+#define DIRECTION	(0x01)
 
 /*
 *  Standard packet for transmitting data
@@ -37,12 +49,12 @@
 */ 
 struct comm_protocol
 {
-	int8_t start_code;	// Constant start byte
-	uint8_t device;		// Device type
-	uint8_t magnitude;	// Device-dependant (i.e. speed of a motor)
-	int8_t flags;		// 
-	int8_t checksum;	// 7-bit checksum & 1-bit checksum parity bit
-	int8_t end_code;	// Constant end byte
+	int8_t 	start_code;	// Constant start byte
+	uint8_t	device;		// Device type
+	uint8_t	magnitude;	// Device-dependant (i.e. speed of a motor)
+	int8_t 	flags;		// 
+	int8_t 	checksum;	// 7-bit checksum & 1-bit checksum parity bit
+	int8_t 	end_code;	// Constant end byte
 };
 
 #endif
