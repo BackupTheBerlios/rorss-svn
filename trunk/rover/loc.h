7158854888ce334b831114b949d6af667effefa5
julians <julians@f5ad59e7-92ee-0310-b632-925698422abb>
2005-10-14 01:31:53 +0000

    Added Masks form p 50

--- trunk/rover/loc.h
+++ trunk/rover/loc.h
@@ -7,3 +7,14 @@
 #include <sdcc/include/stdint.h>
 
 uint8_t at 0xA8 IE;
+
+//More info on P 50 of CC1010 Data Sheet
+uint8_t at 0x80 P0;// only bits 0-3 used
+uint8_t at 0x90 P1;
+uint8_t at 0xA0 P2;
+uint8_t at 0xB0 P3;//only bits 0-5 used
+
+uint8_t at 0xa4 P0DIR;
+uint8_t at 0xa5 P1DIR;
+uint8_t at 0xa6 P2DIR;
+uint8_t at 0xa7 P3DIR;
