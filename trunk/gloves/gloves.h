d1896cbd55bbb4b4cf03b795f604e1dcac379bd4
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2005-11-03 01:38:15 +0000

    Fixed data structure

--- trunk/gloves/gloves.h
+++ trunk/gloves/gloves.h
@@ -6,42 +6,40 @@
 
 #include <sdcc/include/stdint.h>
 
-#define UPPER_NIBBLE	(0xF0)
-#define LOWER_NIBBLE	(0x0F)
+#define HEADER		(0x80)
+
+#define UNFLEXED	(0x00)
+#define FLEXED		(0xFF)
 
 struct glove_serial_data
 {
-	uint8_t		start;		// Defined as 0x3C '<'
-	uint8_t		type;		// 0x10 for right, 0x11 for left
+	uint8_t		header;		// Defined as 0x80
 	
-	uint8_t		version;
-	uint8_t		LI1_HI2;
-	uint8_t		LU2_LI2;
-	uint8_t		HI3_LU3;
-	uint8_t		LI3_HI4;
-	uint8_t		LU4_LI4;
-	uint8_t		HI5_LU5;
-	uint8_t		LI5_HI6;
-	uint8_t		LU6_LI6;
-	uint8_t		HI7_LU7;
-	uint8_t		LI7_HI8;
-	uint8_t		LU8_LI8;
-	uint8_t		HI9_LU9;
-	uint8_t		LI9_HI10;
-	uint8_t		LU10_LI10;
-	uint8_t		HI11_LU11;
-	uint8_t		LI11_HI12;
-	uint8_t		LU12_LI12;
-	uint8_t		HI13_LU13;
-	uint8_t		LI13_HI14;
-	uint8_t		LU14_LI14;
-	uint8_t		HI15_LU15;
-	uint8_t		LI15_HI16;
-	uint8_t		LU16_LI16;
+	// All these are magnitudes from 0-255
+	union
+	{
+		struct right
+		{
+			int8_t		thumb;
+			int8_t		index;
+			int8_t		middle;
+			int8_t		ring;
+			int8_t		little;
+		};
 	
+		struct left
+		{
+			int8_t		little;
+			int8_t		ring;
+			int8_t		middle;
+			int8_t		index;
+			int8_t		thumb;
+		};
+	} hand;
+	
+	int8_t		pitch;
+	int8_t		roll;
 	uint8_t		checksum;
-	uint8_t		footer;		// Defined as 0x3E '>'
-
 };
 
 typedef struct glove_serial_data	GLOVE_SERIAL_DATA;
