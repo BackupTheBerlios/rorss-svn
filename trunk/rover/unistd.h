3859602a01cbbe1e21b747ba78ab0de7d17364a5
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2005-11-14 14:30:54 +0000

    Moved unistd.h to unistd.c

--- trunk/rover/unistd.h
+++ trunk/rover/unistd.h
@@ -5,30 +5,11 @@
  *
  */
 
+#ifndef UNISTD_H
+#define UNISTD_H
+
 #include <sdcc/include/stdint.h>
 
-void delay(uint8_t milliseconds) {
-	uint8_t loop1, loop2;
-	milliseconds;
-	_asm
-		; save registers
-		push a
-		push b
-	outerloop$:
-		; take up ~14741 clock cycles in this loop
-		mov _loop1, 15			; 2 cycles
-	middleloop$:
-		mov _loop2, 244			; 2 cycles
-	innerloop$:
-		djnz _loop2, innerloop$		; 4 cycles
-		djnz _loop1, middleloop$	; 4 cycles
-		djnz _milliseconds, outerloop$	; 4 cycles
-	finish$:
-		; restore registers
-		pop b
-		pop a
-		; return from function
-		ret
-	_endasm;
-}
+void delay(uint8_t);
 
+#endif
