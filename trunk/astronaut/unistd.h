d46e5e908b3513136c765b79d6b8f0102861cb51
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2005-12-28 23:12:33 +0000

    Updated unistd.c

--- trunk/astronaut/unistd.h
+++ trunk/astronaut/unistd.h
@@ -9,7 +9,27 @@
 #define UNISTD_H
 
 #include <sdcc/include/stdint.h>
+#include "loc.h"
 
-void delay(uint8_t);
+#define CLKFREQ 14746
+
+/********** LED macros ***********/
+#define RLED (P1 ^ 0x02) // pin 2
+#define YLED (P1 ^ 0x04) // pin 3
+#define GLED (P1 ^ 0x08) // pin 4
+#define BLED (P2 ^ 0x10) // pin 5
+
+#define LED_ON  0
+#define LED_OFF 1
+
+// LED pin output enable macros
+#define RLED_OE(x) {P1DIR=(x) ? P1DIR&~0x04 : P1DIR|0x04;}
+#define YLED_OE(x) {P1DIR=(x) ? P1DIR&~0x08 : P1DIR|0x08;}
+#define GLED_OE(x) {P1DIR=(x) ? P1DIR&~0x10 : P1DIR|0x10;}
+#define BLED_OE(x) {P2DIR=(x) ? P2DIR&~0x20 : P2DIR|0x20;}
+
+
+/********** Function Prototypes ***********/
+void sleep(uint8_t);
 
 #endif
