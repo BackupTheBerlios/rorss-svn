329969ec46261a95757b11367ee971af86dfead4
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2005-12-28 23:11:12 +0000

    Made glove_serial_data a little smarter

--- trunk/gloves/gloves.h
+++ trunk/gloves/gloves.h
@@ -1,4 +1,5 @@
 /*
+ *
  * $Id$
  * gloves.h - Glove data header file
  *
@@ -20,20 +21,20 @@ struct glove_serial_data
 	{
 		struct right
 		{
-			int8_t		thumb;
-			int8_t		index;
-			int8_t		middle;
-			int8_t		ring;
-			int8_t		little;
+			int8_t		r_thumb;
+			int8_t		r_index;
+			int8_t		r_middle;
+			int8_t		r_ring;
+			int8_t		r_little;
 		};
 	
 		struct left
 		{
-			int8_t		little;
-			int8_t		ring;
-			int8_t		middle;
-			int8_t		index;
-			int8_t		thumb;
+			int8_t		l_little;
+			int8_t		l_ring;
+			int8_t		l_middle;
+			int8_t		l_index;
+			int8_t		l_thumb;
 		};
 	} hand;
 	
