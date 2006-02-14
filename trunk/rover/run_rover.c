007c79530214d52d7528389084ab254beab45265
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2006-02-14 07:00:04 +0000

    Added timer setup and LED test code

--- trunk/rover/run_rover.c
+++ trunk/rover/run_rover.c
@@ -34,18 +34,18 @@ void main(void)
 {
 
 	uint8_t bunk = 255;  // for testing only
-	
+
 	// Initialize serial for 19200 baud, 1 stop bit, no parity
 	CKCON = (CKCON_T1M & 0);	// using table 23 on pg 69
 	TH1   = 252;
 	SCON0 = (SCON_SM0 & 0) | (SCON_SM1 & 1);
-	
+
 	// Set up serial receive
 	SCON1 = (SCON_SM0 & 0) | (SCON_SM1 & 1) | (SCON_REN & 1);
-	
+
 	memset(&incoming_rf_data, 0, sizeof(COMM_PACKET));
 	next_incoming_rf_byte = &(uint8_t) incoming_rf_data;
-	
+
 	// Initialize RF parameters
 	RFCON  = RFCON_BYTEMODE;  // enable bytemode (rcv 8-bits before interrupt fired)
 	PDET   = (PDET_PEN & 0) | (PDET_PLEN & PREAMBLE_LENGTH);
@@ -53,14 +53,23 @@ void main(void)
 				(MODEM0_DATA_FMT & MODEM0_MANCHESTER) | \
 				(MODEM0_XOSC_FREQ & 0x00);
 	RFMAIN = 0;  // enable RX, power everything up
-	
-	IE  = IE_EA | IE_ES1 | IE_ES0;  // enable all interrupts, and serial 0 and 1
+
+	// Setup timer
+	// using clock/12 by default
+	TMOD = 0x02; // Set Mode 4 - two 8-bit counters
+	TCON = TR0;  // Enable timer 0
+
+	IE  = IE_EA | IE_ES1 | IE_ES0 | IE_ET0;  // enable all interrupts, serial 0 and 1, and timer0
 	EIE = EIE_RFIE;  // enable RF interrupt
-	
+
+	// Enable Red LED
+	RLED_OE(true);
+
 	while(1)
 	{
 		// just for testing
 		send_rf_data( &bunk, &bunk + 1 );
+		RLED = RLED ^ LED_ON;  // Toggle Red LED
 		sleep( 500 );
 	}
 
