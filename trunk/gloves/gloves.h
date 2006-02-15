7fd23fdcd0b3d66f101893f5b2de0fc8d11dc2dd
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2006-02-15 06:15:48 +0000

    Added glove_compressed_data

--- trunk/gloves/gloves.h
+++ trunk/gloves/gloves.h
@@ -43,4 +43,23 @@ struct glove_serial_data
 	uint8_t		checksum;
 };
 
+// Each uint in this struct contains two values,
+// each of which is stored in two consecutive nibbles.
+//
+// This means there is room to expand each compressed
+// value to 4 bits, but currently is only 3.
+//
+// Each uint could look like:
+// 01110111
+// if both fingers were flexed
+struct glove_compressed_data
+{
+	uint8_t thumb_and_index;	// && this with 0x70 to get thumb
+					// and 0x07 to get index
+	uint8_t middle_and_ring;
+	uint8_t little_and_pitch;
+	uint8_t roll_and_checksum;
+};
+
 typedef struct glove_serial_data	GLOVE_SERIAL_DATA;
+typedef struct glove_compressed_data	GLOVE_COMPRESSED_DATA;
