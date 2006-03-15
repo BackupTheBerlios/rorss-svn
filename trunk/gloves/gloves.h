270c078fe9e920e845a723c108adac8e9c6df3b1
nixphoeni <nixphoeni@f5ad59e7-92ee-0310-b632-925698422abb>
2006-03-15 06:47:29 +0000

    Changed structure of compressed_data

--- trunk/gloves/gloves.h
+++ trunk/gloves/gloves.h
@@ -12,6 +12,10 @@
 #define UNFLEXED	(0x00)
 #define FLEXED		(0xFF)
 
+#define RIGHT_GLOVE	(0)
+#define LEFT_GLOVE	(1)
+
+
 struct glove_serial_data
 {
 	uint8_t		header;		// Defined as 0x80
@@ -19,29 +23,32 @@ struct glove_serial_data
 	// All these are magnitudes from 0-255
 	union
 	{
-		struct right
+		struct r
 		{
 			int8_t		r_thumb;
 			int8_t		r_index;
 			int8_t		r_middle;
 			int8_t		r_ring;
 			int8_t		r_little;
-		};
+		} right;
 	
-		struct left
+		struct l
 		{
 			int8_t		l_little;
 			int8_t		l_ring;
 			int8_t		l_middle;
 			int8_t		l_index;
 			int8_t		l_thumb;
-		};
+		} left;
 	} hand;
 	
 	int8_t		pitch;
 	int8_t		roll;
 	uint8_t		checksum;
 };
+
+typedef struct glove_serial_data	GLOVE_SERIAL_DATA;
+
 
 // Each uint in this struct contains two values,
 // each of which is stored in two consecutive nibbles.
@@ -54,12 +61,24 @@ struct glove_serial_data
 // if both fingers were flexed
 struct glove_compressed_data
 {
+	/*
 	uint8_t thumb_and_index;	// && this with 0x70 to get thumb
 					// and 0x07 to get index
 	uint8_t middle_and_ring;
 	uint8_t little_and_pitch;
 	uint8_t roll_and_checksum;
+	*/
+	
+	bit hand;
+	
+	bit thumb;
+	bit index;
+	bit middle;
+	bit ring;
+	bit pinky;
+
+	bit pitch;
+	bit roll;
 };
 
-typedef struct glove_serial_data	GLOVE_SERIAL_DATA;
 typedef struct glove_compressed_data	GLOVE_COMPRESSED_DATA;
