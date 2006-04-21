ff376745fef55dde7a38256781a458fb0f2e0a59
pghurwitz <pghurwitz@f5ad59e7-92ee-0310-b632-925698422abb>
2006-04-21 00:24:32 +0000

    Added header file and made appropriate changes

--- trunk/modules/radio_relay/radio_relay.c
+++ trunk/modules/radio_relay/radio_relay.c
@@ -4,14 +4,17 @@
 * data sheet: http://ww1.microchip.com/downloads/en/DeviceDoc/41262A.pdf
 */
 
-#include //something really important
+#include "shared/16F685.h"
+
+#device *=16
+#use rs232(baud=19200, xmit=PIN_B2, rcv=PIN_B1, stream=PC)
 
 //define up-down and open-close bits as registers A3-A0
-#define UD1 (RA3)
-#define UD2 (RA2)
-#define OC1 (RA1)
-#define OC2 (RA0)
-
+#define UD1 (PIN_A3)
+#define UD2 (PIN_A2)
+#define OC1 (PIN_A1)
+#define OC2 (PIN_A0)
+/*
 void open(void);
 void close(void);
 void up(void);
@@ -19,33 +22,55 @@ void down(void);
 void stopUD(void);
 void stopOC(void);
 void stopALL(void);
+*/
+
+#define OPEN() \
+	(output_low(OC1); output_high(OC2))
+#define CLOSE() \
+	(output_low(OC2); output_high(OC1))
+#define UP() \
+	(output_low(UD1); output_high(UD2))
+#define DOWN() \
+	(output_low(UD2); output_high(UD1))
+
+#define STOPOC() \
+	(output_low(OC2); output_low(OC1))
+#define STOPUD() \
+	(output_low(UD2); output_low(UD1))
+#define STOPALL() \
+	(STOPUD(); STOPOC())
+
 
 int main(void)
 {
-	INTCON |= 0xe8;//enable global/tmr0/IOCA interrupts
+	setup_oscillator(OSC_4MHZ);
+	//INTCON |= 0xe8;//enable global/tmr0/IOCA interrupts
+	enable_interrupts(GLOBAL & INT_TIMER0 & INT_RA & INT_RA0 & INT_RA1);
 	PIE1 |= 0x30;//enable TXIE and RCIE on PIE1 (p.29)
 	RCSTA |= 0x90;//enable serial port and continuous recieve enable bit
 
 	//up-down output bits are TRISC 0 and 1
 	//open-close output bits 2 and 3
-	TRISC &= 0x0f;
+	//TRISC &= 0x0f;
+	set_tris_c(0x0f);
 	//up-down input is TRISA 0
 	//open-close input is TRISA 1
-	TRISA &= 0x03;
+	//TRISA &= 0x03;
+	set_tris_a(0x0f);
 
 	//enable interrupt on change A
-	IOCA &= 0x03;
+	//IOCA &= 0x03;
 
-	SYNC = 0;//clearing sync
+	//SYNC = 0;//clearing sync
 	//assuming 4 mhz
-	SPBRG = 2; //19200 baud
+	//SPBRG = 2; //19200 baud
 
-	SPEN = 1; //enabling serial port asynchronously (p.146)
-	RCIE = 1;//enabling interrupts
+	//SPEN = 1; //enabling serial port asynchronously (p.146)
+	//RCIE = 1;//enabling interrupts
 
 	return 0;
 }
-
+/*
 void down(void)
 {
 	UD1 = 0;
@@ -89,3 +114,4 @@ void stopALL(void)
 	OC1 = 0;
 	OC2 = 0;
 }
+*/
